6981253438ecab26ec445bf2e9f02ea66d12c004
TomasDarquier <tomasdarquier@gmail.com>
2024-05-13 10:19:24 -0300

    Se modifico la clase App del metodo exhaustivo para aceptar numeros aleatorios

--- busqueda-exhaustiva/src/App.java
+++ busqueda-exhaustiva/src/App.java
@@ -1,7 +1,13 @@
+import java.util.Random;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        ListaUnidimensional lista = new ListaUnidimensional(40, 0.5);
+        ListaUnidimensional lista = new ListaUnidimensional(70 * 10, 1);
 
-        BFS.bfs(lista, 20, 40);
+        // Se crea una instancia de Random para generar puntos de origen y objetivo aleatorios
+        Random random = new Random();
+
+        BFS.bfs(lista, random.nextInt(701), random.nextInt(701));
     }
 }
+    
