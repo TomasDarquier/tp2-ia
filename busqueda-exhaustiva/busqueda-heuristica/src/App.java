51463d2bb9ff00d2d9287213f1fe6acf21faaca1
TomasDarquier <85067215+TomasDarquier@users.noreply.github.com>
2024-05-13 09:03:08 -0300

    Delete busqueda-exhaustiva/busqueda-heuristica directory

--- busqueda-exhaustiva/busqueda-heuristica/src/App.java
+++
@@ -1,6 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        ListaUnidimensional lista = new ListaUnidimensional(40, 0.5);
-        AEstrella.aEstrella(lista, 21, 32);    
-    }
-}
