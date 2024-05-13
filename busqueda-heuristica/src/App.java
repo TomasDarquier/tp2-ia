64439927629c53c18fe6afd877fea743934e71bf
TomasDarquier <tomasdarquier@gmail.com>
2024-05-13 14:49:41 -0300

    se modifico el main de la busqueda heuristica para permitir la aleatoriedad

--- busqueda-heuristica/src/App.java
+++ busqueda-heuristica/src/App.java
@@ -1,6 +1,13 @@
+import java.util.Random;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        ListaUnidimensional lista = new ListaUnidimensional(40, 0.5);
-        AEstrella.aEstrella(lista, 21, 32);    
+        
+        ListaUnidimensional lista = new ListaUnidimensional(70 * 10, 1);
+
+        // Se crea una instancia de Random para generar puntos de origen y objetivo aleatorios
+        Random random = new Random();
+
+        AEstrella.aEstrella(lista,random.nextInt(701), random.nextInt(701));   
     }
 }
